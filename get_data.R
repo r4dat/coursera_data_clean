13b9d57825beb609b5773531c1a5fba27f2bf4b2
r4dat <rfourtoss@gmail.com>
2014-06-06 08:32:58 -0500

    Formatting change. Make ' = '

--- get_data.R
+++ get_data.R
@@ -32,22 +32,22 @@ if(
 
 # SHOULD PROBABLY JUST CREATE A FUNCTION ACCEPTING STRINGS ("test" and "train")
 # Create train path string.
-tpath=paste(localdir,datadir,"train",sep='/')
-xname="X_train.txt"
-yname="y_train.txt"
-subtname="subject_train.txt"
-xtrain=read.table(file=paste(tpath,'/',xname,sep=''),header=FALSE)
-ytrain=read.table(file=paste(tpath,'/',yname,sep=''),header=FALSE)
-subtrain=read.table(file=paste(tpath,'/',subtname,sep=''),header=FALSE)
+tpath = paste(localdir,datadir,"train",sep='/')
+xname = "X_train.txt"
+yname = "y_train.txt"
+subtname = "subject_train.txt"
+xtrain = read.table(file=paste(tpath,'/',xname,sep=''),header=FALSE)
+ytrain = read.table(file=paste(tpath,'/',yname,sep=''),header=FALSE)
+subtrain = read.table(file=paste(tpath,'/',subtname,sep=''),header=FALSE)
 
 # Create test path string.
-tpath=paste(localdir,datadir,"test",sep='/')
-xname="X_test.txt"
-yname="y_test.txt"
-subtname="subject_test.txt"
-xtest=read.table(file=paste(tpath,'/',xname,sep=''),header=FALSE)
-ytest=read.table(file=paste(tpath,'/',yname,sep=''),header=FALSE)
-subtest=read.table(file=paste(tpath,'/',subtname,sep=''),header=FALSE)
+tpath = paste(localdir,datadir,"test",sep='/')
+xname = "X_test.txt"
+yname = "y_test.txt"
+subtname = "subject_test.txt"
+xtest = read.table(file=paste(tpath,'/',xname,sep=''),header=FALSE)
+ytest = read.table(file=paste(tpath,'/',yname,sep=''),header=FALSE)
+subtest = read.table(file=paste(tpath,'/',subtname,sep=''),header=FALSE)
 
 
 
@@ -77,6 +77,6 @@ colnames(actLabel)[1] = c("activity")
 
 complete$activity = join(x=complete[,1:2],y=actLabel,by="activity")[,3]
 
-
+complete = complete[,grep(pattern="*mean*|*std*",x=xLabel[,2],ignore.case=TRUE)]
 
 
